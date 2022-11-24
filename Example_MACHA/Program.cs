d47fbfb181fae6a6aa14ed2979b9f49c15ddbcc2
Dom <96082010@mail.ru>
2022-11-25 00:10:09 +0300

    009

---
+++ Example_MACHA/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите своё имя");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.WriteLine("fack you");
+}
+else
+{
+    Console.Write("Добро пожаловать,");
+    Console.WriteLine(username);
+}
