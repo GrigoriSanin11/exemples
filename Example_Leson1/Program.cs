a7cf52973a451e38fa50ba8235110457e9b11803
Dom <96082010@mail.ru>
2022-11-25 00:33:31 +0300

    011

---
+++ Example_Leson1/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = 1;
+int B = 8;
+int C = 3;
+int d = 2;
+int e = 6;
+int MAX = A ;
+if (A > MAX) MAX = A;
+if (B > MAX) MAX = B;
+if (C > MAX) MAX = C;
+if (d > MAX) MAX = d;
+if (e > MAX) MAX = e;
+
+Console.Write("max = ");
+Console.WriteLine(MAX);
