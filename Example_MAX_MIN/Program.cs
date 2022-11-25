30b323072eb01cb5388c3b1536a95ab85b2c43fb
Dom <96082010@mail.ru>
2022-11-25 17:26:05 +0300

    013

---
+++ Example_MAX_MIN/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число A") ; 
+int namber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B") ; 
+int namber2 = Convert.ToInt32(Console.ReadLine());
+int max = namber1;
+int min = namber2;
+
+if (max > namber2)
+{
+    max = namber1;
+    min = namber2;
+
+}
+else 
+{
+    max = namber2;
+    min = namber1;
+
+}
+
+Console.Write("max =");
+Console.WriteLine(max );
+
+Console.Write("min =");
+Console.WriteLine( min );
