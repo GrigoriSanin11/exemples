5d6b83a53187e62843efd27f1a8d978b1f596ffc
Dom <96082010@mail.ru>
2022-11-25 17:36:18 +0300

    014

---
+++ Example_MAX_if_3_DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A") ; 
+int namber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B") ; 
+int namber2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C") ; 
+int namber3 = Convert.ToInt32(Console.ReadLine());
+
+int MAX = namber1;
+
+if (namber1 > MAX) MAX = namber1;
+if (namber2 > MAX) MAX = namber2;
+if (namber3 > MAX) MAX = namber3;
+
+Console.Write("MAX = ");
+Console.WriteLine(MAX);
