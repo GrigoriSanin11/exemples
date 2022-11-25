41f8fba42421072db1ffc128e758b9c1e07f914b
Dom <96082010@mail.ru>
2022-11-25 20:54:08 +0300

    111

--- Example_MAX_MIN/Program.cs
+++ Example_MAX_MIN/Program.cs
@@ -6,21 +6,31 @@ int namber2 = Convert.ToInt32(Console.ReadLine());
 int max = namber1;
 int min = namber2;
 
-if (max > namber2)
+if (namber1 == namber2)
+{
+    Console.WriteLine("Числа равны");
+}
+
+else if (max > namber2)
 {
     max = namber1;
     min = namber2;
+    Console.Write("max =");
+    Console.WriteLine(max );
+
+    Console.Write("min =");
+    Console.WriteLine( min );
 
 }
 else 
 {
     max = namber2;
     min = namber1;
+    Console.Write("max =");
+    Console.WriteLine(max );
+
+    Console.Write("min =");
+    Console.WriteLine( min );
 
 }
 
-Console.Write("max =");
-Console.WriteLine(max );
-
-Console.Write("min =");
-Console.WriteLine( min );
